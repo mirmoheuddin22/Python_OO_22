d17b8359f366ce2a120d83baa27ab08db9d2e766
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-12-10 14:26:33 +0100

    Enhanced triangle 1!

---
+++ Rectangle_3.py
@@ -0,0 +1,31 @@
+class Rectangle:
+  def __init__(self, a=10, b=6):
+    self.set_params(a, b)
+
+  def set_params(self, a, par_b):
+    self.__a = a
+    self.b = par_b
+
+  def calc_surface(self):
+    return self.__a * self.b
+
+  def set_a(self, a):
+    self.__a = a
+
+  def get_a(self):
+    return self.__a
+
+  def __repr__(self):
+    return "Rectangle[" + str(self.__a) + " by " + str(self.b) + "] at " + str(hex(id(self)))
+
+
+r = Rectangle(5, 6)
+r.a = 10
+r.__a = 8
+print(r)
+r.set_a(11)
+print(r)
+print(r.calc_surface())
+print('__a in r is: ' + str(r.get_a()))
+print('r.__a in is: ' + str(r.__a))
+print('r.a in is: ' + str(r.a))
