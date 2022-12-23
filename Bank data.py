49514ecff761f918306a89f36ff33ed83335a4bb
Mir Md. Moheuddin <115165307+mirmoheuddin22@users.noreply.github.com>
2022-12-23 23:17:12 +0100

    Banking app - bank with added!

---
+++ Bank data.py
@@ -0,0 +1,75 @@
+class Customer:
+  last_id = 0
+
+  def __init__(self, firstname, lastname):
+    self.firstname = firstname
+    self.lastname = lastname
+    Customer.last_id += 1
+    self.id = Customer.last_id
+
+  def __repr__(self):
+      return f'Customer[{self.id}, {self.firstname}, {self.lastname}]'
+
+class Account:
+  last_id = 1000
+
+  def __init__(self, customer):
+    self.customer = customer
+    Account.last_id += 1
+    self.id = Account.last_id
+    self._balance = 0
+
+  # def deposit(self, amount):
+  #     # TODO Add implementation
+  #     pass
+  #
+  # def charge(self, amount):
+  #   # TODO add implementation
+  #  pass
+
+  def __repr__(self):
+    return f'Acount[{self.id}, {self.customer.lastname}, {self._balance}]'
+
+class Bank:
+  def __init__(self):
+    self.account_list = []
+    self.customer_list = []
+
+  def create_customer(self, firstname, lastname):
+    c = Customer(firstname, lastname)
+    self.customer_list.append(c)
+    return c
+
+  def create_account(self, customer):
+    a = Account(customer)
+    self.account_list.append(a)
+    return a
+
+  def __repr__(self):
+    return f'Bank[{self.customer_list}, {self.account_list}]'
+
+bank = Bank()
+c = bank.create_customer('John', 'Brown')
+print(c)
+a = bank.create_account(c)
+print(a)
+print(bank)
+a1 = Account(c)
+a1 = bank.create_account(c)
+print(a1)
+a2 = Account(c)
+print(a2)
+a2 = bank.create_account(c)
+print(a2)
+c2 = bank.create_customer('Anne', 'Smith')
+a3 = bank.create_account(c2)
+print(bank)
+
+# c = Customer('John', 'Brown')
+# print(c)
+# a = Account(c)
+# print(a)
+
+
+
+
